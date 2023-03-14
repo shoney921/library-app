638fc9de8d8d05e2beb6e6d4c3eb39a47fb967e7
shoney <ddmczp@naver.com>
2023-03-14 23:21:33 +0900

    [feat] UserController 추가

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,14 +1,40 @@
 package com.group.libraryapp.controller.user;
 
+import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    @PostMapping("/user")
-    public void saveUser() {
+    private final List<User> users = new ArrayList<>();
 
+    @PostMapping("/user")
+    public void saveUser(@RequestBody UserCreateRequest request) {
+        users.add(new User(request.getName(), request.getAge()));
     }
 
+    @GetMapping("/users")
+    public List<User> searchUsers() {
+        return users; //return을 객체로 할 경우 getter 없으면 json로 변환 안됨
+    }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUsers() {
+        List<UserResponse> responses = new ArrayList<>();
+        long id = 0;
+
+        for (User user : users) {
+            responses.add(new UserResponse(++id, user));
+        }
+
+        return responses;
+    }
 }
